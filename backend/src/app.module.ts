43c754e4439c7ef109306f0e6b4cb5cb25346872
guigs <guidentzien@hotmail.com>
2025-08-20 17:18:27 -0300

    Configuração do Prisma integrando com o NestJS: Schema criado Módulo e Service do Prisma criados Integração do Prisma no AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DeputadosModule } from './deputados/deputados.module';
 import { DesesasModule } from './desesas/desesas.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DeputadosModule, DesesasModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DeputadosModule,
+    DesesasModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
